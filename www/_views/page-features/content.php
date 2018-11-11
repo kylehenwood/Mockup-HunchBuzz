cc0ce5361376da3ecb471e78fa6ea5cf2978f570
kyle <kyle.henwood@gmail.com>
2018-11-11 23:23:42 +1300

    done for now

--- www/_views/page-features/content.php
+++ www/_views/page-features/content.php
@@ -3,18 +3,75 @@
   <div class="center center--1040">
 
     <div class="feature-primary clearfix">
-      <div class="feature-primary__item"></div>
-      <div class="feature-primary__item"></div>
-      <div class="feature-primary__item"></div>
+      <div class="feature-primary__item">
+        <div class="primary-feature">
+          <h4 class="primary-feature__title">Simple to Use.</h4>
+          <p class="primary-feature__desc">Quickly create and contribute to ideas with no training required. Challenge and idea KanBan cards with custom defined images. Admins manage users, security, challenges, design and more through a simple dashboard.</p>
+          <div class="primary-feature__image"></div>
+          <div class="primary-feature__icon"></div>
+        </div>
+      </div>
+      <div class="feature-primary__item">
+        <div class="primary-feature">
+          <h4 class="primary-feature__title">Hosted in the Cloud.</h4>
+          <p class="primary-feature__desc">HunchBuzz is a cloud hosted service - that means no software for you to deploy and manage. A typical implementation can be up and running within minutes rather than days.</p>
+          <div class="primary-feature__image"></div>
+          <div class="primary-feature__icon"></div>
+        </div>
+      </div>
+      <div class="feature-primary__item">
+        <div class="primary-feature">
+          <h4 class="primary-feature__title">Make it your own.</h4>
+          <p class="primary-feature__desc">Upload your logo and choose colors that match your brand perfectly. Display custom pages to provide rich context. Use the built in blog and news capabilities to communicate with your community.</p>
+          <div class="primary-feature__image"></div>
+          <div class="primary-feature__icon"></div>
+        </div>
+      </div>
     </div>
 
     <div class="feature-secondary clearfix">
-      <div class="feature-secondary__item"></div>
-      <div class="feature-secondary__item"></div>
-      <div class="feature-secondary__item"></div>
-      <div class="feature-secondary__item"></div>
-      <div class="feature-secondary__item"></div>
-      <div class="feature-secondary__item"></div>
+      <div class="feature-secondary__item">
+        <div class="secondary-feature">
+          <h4 class="secondary-feature__title">Structured idea generation.</h4>
+          <p class="secondary-feature__desc">Create focused challenges to guide the idea generation process. Contribute ideas, feedback, comments, reviews and votes. Define custom idea lifecycle stages to relect your organizational workflow.</p>
+          <div class="secondary-feature__icon"></div>
+        </div>
+      </div>
+      <div class="feature-secondary__item">
+        <div class="secondary-feature">
+          <h4 class="secondary-feature__title">Mobile.</h4>
+          <p class="secondary-feature__desc">Join the conversation from any computer, tablet or mobile device. Download our mobile app for any iOS or Android device.</p>
+          <div class="secondary-feature__icon"></div>
+        </div>
+      </div>
+      <div class="feature-secondary__item">
+        <div class="secondary-feature">
+          <h4 class="secondary-feature__title">Single Sign-On.</h4>
+          <p class="secondary-feature__desc">Integrate with your organization's existing directory (ADFS/SAML) for seamless single sign-on.</p>
+          <div class="secondary-feature__icon"></div>
+        </div>
+      </div>
+      <div class="feature-secondary__item">
+        <div class="secondary-feature">
+          <h4 class="secondary-feature__title">Drive User Engagement.</h4>
+          <p class="secondary-feature__desc">Built in gamfication allows users to earn points and redeem rewards. Incent user loyalty through a leaderboard, favorites and subscribing, activity notifications and achievements (badges).</p>
+          <div class="secondary-feature__icon"></div>
+        </div>
+      </div>
+      <div class="feature-secondary__item">
+        <div class="secondary-feature">
+          <h4 class="secondary-feature__title">Rich Reporting and Analytics.</h4>
+          <p class="secondary-feature__desc">See at a glance what ideas are trending over time. Create customizable, graphical reports on user engagement, site activity, achievements and more.</p>
+          <div class="secondary-feature__icon"></div>
+        </div>
+      </div>
+      <div class="feature-secondary__item">
+        <div class="secondary-feature">
+          <h4 class="secondary-feature__title">Work Like You Do.</h4>
+          <p class="secondary-feature__desc">Create users and groups that map to your org structure. Challenge public/private settings to provide access to specific groups. Use sub-communities for specialized functional areas.</p>
+          <div class="secondary-feature__icon"></div>
+        </div>
+      </div>
     </div>
 
   </div>
