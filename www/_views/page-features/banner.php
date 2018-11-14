0efd7e9fd1239d843f4cafbe3f935147e3049a67
kyle <kyle.henwood@gmail.com>
2018-11-14 16:37:00 +1300

    responsive updates

--- www/_views/page-features/banner.php
+++ www/_views/page-features/banner.php
@@ -1,38 +1,37 @@
 <!-- Banner -->
-<div class="page__banner">
-	<div class="layout__banner">
 
+<div class="layout__banner">
+
+	<div class="features-banner">
 		<div class="center center--1040">
-	    <div class="features-banner">
 
-				<div class="features-banner__illustration">
-					<?php include './_components/illustration-innovation.php';?>
+			<div class="features-banner__illustration">
+				<?php include './_components/illustration-innovation.php';?>
+			</div>
+
+			<div class="features-banner__content">
+  			<h1 class="features-banner__title">Features that fit.</h1>
+				<div class="features-banner__border"></div>
+				<div class="features-banner__subtitle">
+				HunchBuzz was built with employees in mind, we know sharing ideas in organisations filled with competition is hard and you don’t always know who to talk to.
 				</div>
-
-				<div class="features-banner__content">
-	  			<h1 class="features-banner__title">Features that fit.</h1>
-					<div class="features-banner__border"></div>
-					<div class="features-banner__subtitle">
-					HunchBuzz was built with employees in mind, we know sharing ideas in organisations filled with competition is hard and you don’t always know who to talk to.
-					</div>
-					<div class="features-banner__actions">
-						<div class="features-banner__signup">
-							<a class="button button--56 button--white button--shadow">
-								<div class="button-inner button-inner--icon-after">
-									<div class="button-inner__text">Sign up</div>
-									<div class="button-inner__icon-after">
-										<span class="flat-icon icon--16 icon--arrow-right icon--blue"></span>
-									</div>
+				<div class="features-banner__actions">
+					<div class="features-banner__signup">
+						<a class="button button--56 button--white button--shadow">
+							<div class="button-inner button-inner--icon-after">
+								<div class="button-inner__text">Sign up</div>
+								<div class="button-inner__icon-after">
+									<span class="flat-icon icon--16 icon--arrow-right icon--blue"></span>
 								</div>
-							</a>
-						</div>
-						<div class="features-banner__demo">
-							<a class="button button--56 button--transparent-stroke">Request demo</a>
-						</div>
+							</div>
+						</a>
+					</div>
+					<div class="features-banner__demo">
+						<a class="button button--56 button--transparent-stroke">Request demo</a>
 					</div>
 				</div>
-	    </div>
-		</div>
-
+			</div>
+    </div>
 	</div>
+
 </div>
