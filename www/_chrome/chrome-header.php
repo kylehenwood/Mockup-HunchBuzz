d9b71eac416a64e894992e8c3059eb953b339c36
kyle <kyle.henwood@gmail.com>
2018-11-16 17:00:11 +1300

    mobile navigation icons

--- www/_chrome/chrome-header.php
+++ www/_chrome/chrome-header.php
@@ -27,7 +27,14 @@
         </div>
       </div>
 
-      <div class="header__mobile-button js-mobile-toggle"></div>
+      <div class="header__mobile-button js-mobile-toggle">
+        <svg width="32px" height="32px" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+          <g id="icon--menu" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+              <rect id="bounds" x="0" y="0" width="32" height="32"></rect>
+              <path d="M7.5,23 L24.5,23 C25.3284271,23 26,23.6715729 26,24.5 C26,25.3284271 25.3284271,26 24.5,26 L7.5,26 C6.67157288,26 6,25.3284271 6,24.5 C6,23.6715729 6.67157288,23 7.5,23 Z M7.5,14.5 L24.5,14.5 C25.3284271,14.5 26,15.1715729 26,16 C26,16.8284271 25.3284271,17.5 24.5,17.5 L7.5,17.5 C6.67157288,17.5 6,16.8284271 6,16 C6,15.1715729 6.67157288,14.5 7.5,14.5 Z M7.5,6 L24.5,6 C25.3284271,6 26,6.67157288 26,7.5 C26,8.32842712 25.3284271,9 24.5,9 L7.5,9 C6.67157288,9 6,8.32842712 6,7.5 C6,6.67157288 6.67157288,6 7.5,6 Z" fill="#FFFFFF"></path>
+          </g>
+        </svg>
+      </div>
     </div>
 
   </div>
@@ -38,7 +45,14 @@
 <div class="layout__mobile-navigation js-mobile-navigation">
   <div class="mobile-navigation">
 
-    <div class="mobile-navigation__close js-mobile-toggle"></div>
+    <div class="mobile-navigation__close js-mobile-toggle">
+      <svg width="32px" height="32px" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+          <g id="icon--close" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+              <rect id="bounds" x="0" y="0" width="32" height="32"></rect>
+              <path d="M14.5,14.5 L14.5,7.5 C14.5,6.67157288 15.1715729,6 16,6 C16.8284271,6 17.5,6.67157288 17.5,7.5 L17.5,14.5 L24.5,14.5 C25.3284271,14.5 26,15.1715729 26,16 C26,16.8284271 25.3284271,17.5 24.5,17.5 L17.5,17.5 L17.5,24.5 C17.5,25.3284271 16.8284271,26 16,26 C15.1715729,26 14.5,25.3284271 14.5,24.5 L14.5,17.5 L7.5,17.5 C6.67157288,17.5 6,16.8284271 6,16 C6,15.1715729 6.67157288,14.5 7.5,14.5 L14.5,14.5 Z" fill="#1976D2" transform="translate(16.000000, 16.000000) rotate(45.000000) translate(-16.000000, -16.000000) "></path>
+          </g>
+      </svg>
+    </div>
 
     <a class="mobile-navigation__item" href="index.php">Home</a>
     <a class="mobile-navigation__item" href="index.php?pageID=features">Features</a>
